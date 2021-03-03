c27ca8d2d9d5c3906c4633defe02a4cd8922b032
taylorlallas <tlallas@stanford.edu>
2021-03-02 20:37:39 -0800

    Confirmation screen updated

--- App/Screens/RecordScreens/Confirmation.js
+++ App/Screens/RecordScreens/Confirmation.js
@@ -5,15 +5,31 @@ import BackgroundGradient from '../../Components/BackgroundGradient.js';
 import LongButton from '../../Components/LongButton.js';
 import { Images } from '../../Themes/index.js';
 import { useNavigation } from '@react-navigation/native';
+import { ImageBackground } from 'react-native';
     
     
 export default function Confirmation () {
     const navigation = useNavigation();
     return (
-        <View style ={styles.container}>
-            <BackgroundGradient/>
-            <Button title={'Record New Story'} onPress={() => {navigation.navigate('RecordHome')}}/>
-        </View>
+        <SafeAreaView>
+            
+            <ImageBackground source={Images.santaMonica} style={styles.image}>
+            <View style ={styles.container}>
+                <TouchableOpacity style={styles.recordNew}  onPress={() => {navigation.navigate('RecordHome')}}>
+                    <Text style={styles.recordButtonText}>Record New Story</Text>
+                </TouchableOpacity>
+
+
+                <Image source={Images.storyBubble} style={styles.storyBubble}>
+
+                </Image>
+            </View>
+            </ImageBackground>
+
+        
+
+        </SafeAreaView>
+        
     );
 
 }
@@ -22,8 +38,28 @@ export default function Confirmation () {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       alignItems: 'center',
+      flexDirection: 'column',
     },
+    image: {
+        height: '100%',
+        width: '100%'
+      },
+    recordNew: {
+        backgroundColor: 'white',
+        borderRadius: 15,
+        padding: "3%",
+        borderColor: "#F1B600",
+        borderWidth: 3,
+        marginTop: '80%'
+    },
+    storyBubble: {
+        marginTop: '35%',
+        marginRight: '40%'
+    },
+    recordButtonText: {
+        fontSize: 20
+    }
 });
 
--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,7 +1,9 @@
 // leave off @2x/@3x
 const images = {
   blueOrb : require('../Images/blue-orb.png'),
-  yellowOrb : require('../Images/yellow-orb.png')
+  yellowOrb : require('../Images/yellow-orb.png'),
+  santaMonica : require('../Images/santaMonica.png'),
+  storyBubble : require('../Images/smStory.png')
 }
 
 export default images

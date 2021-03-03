e04ba2a03a93cacfc6940b5349ee653942a696c1
Baker Sharp <bakers@stanford.edu>
2021-03-03 16:55:17 -0500

    pending changes to fix gitignore weirdness

--- App/Screens/RecordScreens/EditStory.js
+++ App/Screens/RecordScreens/EditStory.js
@@ -1,29 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, View, AsyncStorage, Image, TouchableOpacity, FlatList, Button } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import BackgroundGradient from '../../Components/BackgroundGradient.js';
-import LongButton from '../../Components/LongButton.js';
-import { Images } from '../../Themes/index.js';
+import { 
+    StyleSheet, 
+    Text, 
+    SafeAreaView, 
+    View,
+    Button 
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-    
+import Trimmer from 'react-native-trimmer'
+
+import BackgroundGradient from '../../Components/BackgroundGradient.js';    
     
 export default function EditStory () {
     const navigation = useNavigation();
+    const [leftHandlePos, setLeftHandlePos] = useState(0);
+    const [rightHandlePos, setRightHandlePos] = useState(0);
+
+    onHandleChange = ({ leftPosition, rightPosition }) => {
+        setLeftHandlePos(leftPosition);
+        setRightHandlePos(rightPosition);
+    }
+
     return (
         <View style ={styles.container}>
             <BackgroundGradient/>
+            <Trimmer
+                onHandleChange={onHandleChange}
+                totalDuration={60000}
+                trimmerLeftHandlePosition={leftHandlePos}
+                trimmerRightHandlePosition={rightHandlePos}
+            />
             <Button title={'Story Info'} onPress={() => {navigation.navigate('StoryInfo')}}/>
         </View>
     );
-
 }
 
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 });
 

3b037d0f0eec02be8360a463c03312f97fcd3080
Krishnan Nair <65053911+KrishnanNair19@users.noreply.github.com>
2021-03-03 16:42:34 -0600

    story list component added!

--- App/Components/LocationInfo.js
+++ App/Components/LocationInfo.js
@@ -1,20 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, SafeAreaView, View, AsyncStorage, Image, TouchableOpacity, FlatList, Button, ImageBackground } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, AsyncStorage, Image, TouchableOpacity, FlatList, Button, ImageBackground, ScrollView} from 'react-native';
 import { Images } from '../Themes';
-export default function LocationInfo () {
-    return (
-        <View style={styles.container}>
-            <Text>99 Stories Available</Text>
-            <View style={styles.background}>
-                <ImageBackground source={Images.yellowOrb} resizeMode={'contain'} style={styles.orb}>
-                    <Image source={Images.parliament} resizeMode='contain' style={styles.locationImage}/>
-                </ImageBackground>
-                
-            </View>
-            
+import StoryClip from './StoryClip.js';
 
-        </View>
-    )
+export default function LocationInfo () {
+
+    const DATA = [
+        {
+          id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+          title: "First Item",
+        },
+        {
+          id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+          title: "Second Item",
+        },
+        {
+          id: "58694a0f-3da1-471f-bd96-145571e29d72",
+          title: "Third Item",
+        },
+        {
+        id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+        title: "First Item",
+        },
+        {
+        id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+        title: "Second Item",
+        },
+        {
+        id: "58694a0f-3da1-471f-bd96-145571e29d72",
+        title: "Third Item",
+        }
+    ];
+
+    const renderItem = () => {
+        return (
+            <StoryClip/>
+        );
+
+    }
+
+
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Text>99 Stories Available</Text>
+                <View style={styles.background}>
+                    <ImageBackground source={Images.yellowOrb} resizeMode={'contain'} style={styles.orb}>
+                        <Image source={Images.parliament} resizeMode='contain' style={styles.locationImage}/>
+                    </ImageBackground>             
+                </View>
+                
+                <View style={styles.flatlist}>
+                    <FlatList
+                        data={DATA}
+                        renderItem={renderItem}
+                        keyExtractor={(item) => item.id}
+                    />
+
+                </View>
+                
+
+            </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -45,6 +93,11 @@ const styles = StyleSheet.create({
         height: '80%',
         aspectRatio: 1,
     
+    },
+    flatlist: {
+        marginTop: 20,
+        flex: 1,
+        width:'100%'
     }
   
 
---
+++ App/Components/StoryClip.js
@@ -0,0 +1,139 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {Ionicons, MaterialIcons} from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+
+export default function StoryClip() {
+  let [fontsLoaded] = useFonts({
+    'NotoSansRegular': require('../Fonts/NotoSansMalayalam-Regular.ttf'),
+    'NotoSansBold': require('../Fonts/NotoSansMalayalam-Bold.ttf'),
+  }, []);
+
+  return (
+      <View style={styles.storyInList}>
+
+        <View style={styles.topRow}>
+          <View style={styles.playColumn}>
+            <Ionicons name={'play-circle'} color={'#1ddbb5'} size={56}/>
+          </View>
+          <View style={styles.titleColumn}>
+            <Text style={styles.title}>Story Title</Text>
+          </View>
+          <View style={styles.rightColumn}>
+            <Text>MTH 2099</Text>
+          </View>
+          
+        </View>
+          
+
+        <View style={styles.bottomRow}>
+          <View style={styles.playColumn}>
+            <Text> 99 min </Text>
+          </View>
+          <View style={styles.titleColumn}>
+            <View style={styles.tagRow}>
+              <View style={styles.tag}>
+                  <Text>Tag 1</Text>
+                </View>
+                <View style={styles.tag}>
+                  <Text>Tag 2</Text>
+                </View>
+              </View>
+            </View>
+          <View style={styles.rightColumn}>
+              <View style={styles.sharingIconsRow}>
+                <Ionicons name={"md-share-outline"} size={24} color={'black'}/>
+                <MaterialIcons name={"playlist-add"} size={30} color={'black'}/>
+              </View>
+          </View>
+        
+        </View>
+      </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  storyInList: {
+    alignSelf: 'center',
+    height: 115,
+    width: '100%',
+    borderTopWidth: 2,
+    borderColor: 'grey',
+    // borderWidth: 2,
+    
+    flexDirection: 'column',
+    padding: 10
+  }, 
+  playColumn: {
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+ 
+  },
+  titleColumn: {
+
+    flexDirection: 'column',
+    flex: 3,
+    paddingLeft: 8,
+    height: '100%',
+   
+    justifyContent: 'center',
+  },
+  rightColumn: {
+
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flex: 1,
+    height: '100%',
+    // backgroundColor: 'yellow'
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'NotoSansBold',
+    fontWeight: 'bold'
+  },
+  tagRow: {
+    flexDirection: 'row', 
+    
+
+  },
+  tag: {
+    borderRadius: 15,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 3,
+    paddingHorizontal: 15,
+    marginRight: 5
+  },
+  sharingIconsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  topRow: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    alignItems: 'center',
+
+  },
+  bottomRow: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+
+  }
+  
+});

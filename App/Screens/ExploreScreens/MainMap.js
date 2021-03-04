8fb371e252000dd93dd43ba22f911998dea4595c
Krishnan Nair <65053911+KrishnanNair19@users.noreply.github.com>
2021-03-04 15:58:48 -0600

    first stab at map failed

--- App/Screens/ExploreScreens/MainMap.js
+++ App/Screens/ExploreScreens/MainMap.js
@@ -2,24 +2,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, SafeAreaView, View, AsyncStorage, Image, TouchableOpacity, FlatList, Button } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+// import MapView from 'react-native-maps';
 
 export default function MainMap() {
     const navigation = useNavigation();
     return (
         <View style={styles.container}>
-            <Text>
-                This screen will be the main explore screen. The following needs to be implemented. 
+            
 
-                1. Scrollable Map
-
-                2. SearchBar/DropDown list of available locations
-
-                3. Clickable pictures of locations with stories. 
-
-                4. Recenter button
-
-            </Text>
-
+           
             <Button title={'StoryList'} onPress={() => {navigation.navigate('StoryList')}}/>
         </View>
     );

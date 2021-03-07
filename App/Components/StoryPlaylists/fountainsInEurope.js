848905358d3f65cafac78e7f0c07d8841736ce7e
adrazen <adrazen>
2021-03-07 01:10:17 -0500

    added playlists data

---
+++ App/Components/StoryPlaylists/fountainsInEurope.js
@@ -0,0 +1,62 @@
+export const newYorkPlaylist = [
+    {
+      id: "1",
+      title: "The History of Trevi Fountain",
+      length: 32, 
+      date: "Jan 2020", 
+      author: '',
+      tags: ["History"],
+      coordinates: { 
+        latitude: 41.9009, 
+        longitude: 12.4833
+        }
+    },
+    {
+        id: "2",
+        title: "London's Gem: The Fountains at Trafalgar Square",
+        length: 50, 
+        date: "Feb 2018", 
+        author: '',
+        tags: ["Architecture", "Community"],
+        coordinates: { 
+          latitude: 51.5080, 
+          longitude: 0.1281
+          }
+      },
+    {
+      id: "3",
+      title: "Jet d'Eau",
+      length: 19, 
+      date: "Mar 2019", 
+      author: '',
+        tags: ["Art"],
+        coordinates: { 
+          latitude: 46.2074, 
+          longitude: 6.1559
+          }
+    },
+    {
+        id: "4",
+        title: "Poland in Paris: Fountain of Warsaw",
+        length: 33, 
+        date: "May 2017", 
+        author: '',
+        tags: ["Nature", "Architecture", "Art"],
+        coordinates: { 
+            latitude: 48.8616, 
+            longitude: 2.2893
+            }
+    },
+    {
+        id: "5",
+        title: "A Piece of Nature and Quiet",
+        length: 8, 
+        date: "Dec 2019", 
+        author: '',
+        tags: ["Art", "Nature", "Love"],
+        coordinates: { 
+            latitude: 51.3167, 
+            longitude: 9.4046
+        }
+    }
+];
---
+++ App/Components/StoryPlaylists/index.js
@@ -0,0 +1,22 @@
+import { newYorkPlaylist } from "./newYorkPlaylist";
+import {utahToBelizePlaylist} from "./utahToBelizePlaylist";
+import {newYorkPlaylist} from "./newYorkPlaylist";
+import {fountainsInEurope} from "./fountainsInEurope";
+
+const playlists = [
+  {
+    title: "Memories From Utah to Belize",
+    stories: utahToBelizePlaylist,
+  },
+  {
+    title: "Adventures in New York",
+    stories: newYorkPlaylist,
+  },
+  {
+    title: "Fountains in Europe",
+    stories: fountainsInEurope,
+  },
+  
+];
+
+export default storyLocations; 

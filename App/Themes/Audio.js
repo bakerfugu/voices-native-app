61b5b9bf7150b6e6f0bb3cffc74d87fecc000c2b
taylorlallas <tlallas@stanford.edu>
2021-03-15 16:26:44 -0700

    creation of audio.js

--- App/Themes/Audio.js
+++ App/Themes/Audio.js
@@ -1,4 +1,45 @@
-const audio = [
+const audio = {
+    //Barcelona
+    fcBarca : require('../Audio/fcBarca.mp3'),
+    gaudiBehindMasterpiece : require('../Audio/AntoniGaudiTheManBehindtheMasterpiece.mp3'),
 
-]
+    //Sagrada familia
+    gaudiSagrada : require('../Audio/GaudiSagradaFamilia.mp3'),
+    inspiration : require('../Audio/InspirationfromLaSagradaFamilia.mp3'),
+    notGaudiFan : require('../Audio/notGaudiFan.mp3'),
+    laSagradaGhosts : require('../Audio/laSagradaGhosts.mp3'),
+
+    //La Cascada 
+    makingOfLaCascada : require('../Audio/makingLaCascada.mp3'),
+    oasis : require('../Audio/oasisAmidstUncertainty.mp3'),
+    blockedAccess : require('../Audio/blockedAccess.mp3'),
+
+    //Razmatazz
+    razmatazz : require('../Audio/razzmatazzBarca.mp3'),
+
+    //Teleferic 
+    teleferic : require('../Audio/Teleferic.mp3'),
+
+
+
+    //SF
+    //Bay to breakers
+    tradition : require('../Audio/baytobreakers.mp3'),
+    loveBaytoBreakers : require('../Audio/ILoveBtB.mp3'),
+
+    //crossroads
+    crossroads : require('../Audio/crossroadsStory.mp3'),
+
+    //Hippie Hill
+    swingWithView : require('../Audio/swingHippieHill.mp3'),
+    hippieCulture : require('../Audio/HippieHillCulture.mp3'),
+    hippieHistory: require('../Audio/HistoryofHippieHill.mp3'),
+
+    //Smitten
+    iceCreamTour : require('../Audio/iceCreamTourExtravaganza.mp3'),
+    learnAboutSmitten : require('../Audio/LearnaboutSmittenIceCream.mp3'),
+    smittenOwner : require('../Audio/MeettheOwnerofSmittenIceCream.mp3'),
+    moreThanIceCreamShop : require('../Audio/MoreThanIceCreamShop.mp3'),
+    smittenIceCream : require('../Audio/smittenIceCream.mp3'),
+};
 export default audio;

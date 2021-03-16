a35ef8b04c742833a95e57f27d3b765fc91539e8
adrazen <adrazen>
2021-03-15 22:34:14 -0400

    playlist data

--- App/Components/StoryLocations/barcelonaTeleferic.js
+++ App/Components/StoryLocations/barcelonaTeleferic.js
@@ -9,5 +9,6 @@ export const barcelonaTeleferic = [
       tags: ["Food"], 
       author: "tlallas", 
       image: Images.teleferic,
-      transcript: `Teleferic barcelona has some of the best tapas you can find.  Like many restaurants in the city it opens for dinner around 8pm.  You cannot leave without trying the grilled octopus. It is to die for. This family owned and operated restaurant has made a name for itself worldwide. Teleferic also has locations in San Francisco and Palo Alto California— though I think the original Barcelona location is definitely the best! `},
-];
+      transcript: `Teleferic barcelona has some of the best tapas you can find.  Like many restaurants in the city it opens for dinner around 8pm.  You cannot leave without trying the grilled octopus. It is to die for. This family owned and operated restaurant has made a name for itself worldwide. Teleferic also has locations in San Francisco and Palo Alto California— though I think the original Barcelona location is definitely the best! `
+    },  
+    ];
--- App/Components/StoryPlaylists/index.js
+++ App/Components/StoryPlaylists/index.js
@@ -2,23 +2,29 @@ import { newYorkPlaylist } from "./newYorkPlaylist";
 import {utahToBelizePlaylist} from "./utahToBelizePlaylist";
 import {fountainsInEurope} from "./fountainsInEurope";
 import {storiesFromBarcelona} from "./storiesFromBarcelona";
+import {sweetMemoriesFromSF} from "./sweetMemoriesFromSF";
 import {Images} from '../../Themes';
 
 const storyPlaylists = [
-  {
-    title: "Memories From Utah to Belize",
-    stories: utahToBelizePlaylist,
-    image: utahToBelizePlaylist[0].image,
-  },
-  {
-    title: "Fountains in Europe",
-    stories: fountainsInEurope,
-    image: fountainsInEurope[0].image,
-  },
+  // {
+  //   title: "Memories From Utah to Belize",
+  //   stories: utahToBelizePlaylist,
+  //   image: utahToBelizePlaylist[0].image,
+  // },
+  // {
+  //   title: "Fountains in Europe",
+  //   stories: fountainsInEurope,
+  //   image: fountainsInEurope[0].image,
+  // },
   {
     title: "Stories from Barcelona",
     stories: storiesFromBarcelona,
     image: storiesFromBarcelona[0].image,
+  },
+  {
+    title: "Sweet Memories from San Francisco",
+    stories: sweetMemoriesFromSF,
+    image: sweetMemoriesFromSF[0].image,
   },
   
 ];
